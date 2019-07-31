2352631d666c942f9102aac5e17adc8a0a2edc0e
msos <marcsoscambras@gmail.com>
2019-07-31 18:31:50 +0200

    Working PDF reader

--- Shifts.py
+++ Shifts.py
@@ -3,7 +3,8 @@ import PyPDF2
 
 # Object creation
 
-pdfFileObj = open('Z:\Calendars\Operation2020.pdf','rb')
+pdfFileObj = open('Operation2020.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pageObj = pdfReader.getPage(0)
 Raw_Calendar = (pageObj.extractText())
+print (Raw_Calendar)
