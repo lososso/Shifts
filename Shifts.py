cc7e1b082d18d4db461e9ee415fedd8e10e85e1d
msos <marcsoscambras@gmail.com>
2019-07-31 18:30:50 +0200

    Working PDF reader

--- Shifts.py
+++ Shifts.py
@@ -7,8 +7,3 @@ pdfFileObj = open('Z:\Calendars\Operation2020.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pageObj = pdfReader.getPage(0)
 Raw_Calendar = (pageObj.extractText())
-
-
-
-
-
