b5efcd75c96fefc70af22aeaf6672b305dbdf1cc
msos <marcsoscambras@gmail.com>
2019-08-01 15:39:50 +0200

    Working PDF reader 2

--- read_PDF.py
+++ read_PDF.py
@@ -7,4 +7,6 @@ pdfFileObj = open('Operation2020.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pageObj = pdfReader.getPage(0)
 Raw_Calendar = (pageObj.extractText())
-print (Raw_Calendar)
+Raw_Calendar = str(Raw_Calendar)
+print (Raw_Calendar.split('\n'))
+
