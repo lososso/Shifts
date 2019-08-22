31337dfd5432a40965a08ba8be0d0f2b07a449c6
lososso <52547966+lososso@users.noreply.github.com>
2019-08-22 13:13:39 +0200

    color buttons

--- ShiftGUI.py
+++ ShiftGUI.py
@@ -25,19 +25,45 @@ def create_gui(year_list,calendar_dict):
     w.setWindowTitle("Shift Gui V0.1")
     for index, element in enumerate(year_list):
         label = QLabel(element)
-        main_layout.addWidget(label, 0, (index * 3))
+        main_layout.addWidget(label, 0, (index))
         month = calendar_dict[element]
         for elements in month:
             day = elements[0]
             morning = elements[1]
             evening = elements[2]
             night = elements[3]
-            morning_label = QLabel(morning)
-            evening_label = QLabel(evening)
-            night_label = QLabel(night)
-            main_layout.addWidget(morning_label,day,(index))
-            main_layout.addWidget(evening_label,day,(index * 2))
-            main_layout.addWidget(night_label,day,(index*3))
+            shift_layout = QGridLayout()
+            morning_label = QPushButton(morning)
+            if morning == 'BL':
+                morning_label.setStyleSheet("background-color: orange")
+            if morning == 'M':
+                morning_label.setStyleSheet("background-color: green")
+            if morning == 'W':
+                morning_label.setStyleSheet("background-color: cyan")
+            if morning == 'OFF':
+                morning_label.setStyleSheet("background-color: yellow")
+            evening_label = QPushButton(evening)
+            if evening == 'BL':
+                evening_label.setStyleSheet("background-color: orange")
+            if evening == 'M':
+                evening_label.setStyleSheet("background-color: green")
+            if evening == 'W':
+                evening_label.setStyleSheet("background-color: cyan")
+            if evening == 'OFF':
+                evening_label.setStyleSheet("background-color: yellow")
+            night_label = QPushButton(night)
+            if night == 'BL':
+                night_label.setStyleSheet("background-color: orange")
+            if night == 'M':
+                night_label.setStyleSheet("background-color: green")
+            if night == 'W':
+                night_label.setStyleSheet("background-color: cyan")
+            if night == 'OFF':
+                night_label.setStyleSheet("background-color: yellow")
+            main_layout.addLayout(shift_layout,day,index)
+            shift_layout.addWidget(morning_label,0,1)
+            shift_layout.addWidget(evening_label,0,2)
+            shift_layout.addWidget(night_label,0,3)
 
     for index in range (1,32):
         label = QLabel(str(index))
